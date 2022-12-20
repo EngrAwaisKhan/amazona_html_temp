8340826dc67fd1eb449a212ac61041386bbe0d8d
Muhammad Awais Khan <66188894+EngrAwaisKhan@users.noreply.github.com>
2022-12-20 13:19:57 +0500

    section-12(Video-13-Add-Redux-To-Product-Screen)

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,17 @@ import data from './data.js';
 
 const app = express();
 
+// ***PRODUCT API***
+// API for fetching Product by Id
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+// API for fetching All Product
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,18 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer,initialState, composeEnhancer(applyMiddleware(thunk)),);
 const store = configureStore({
   reducer: reducer,
   initialState: initialState,
