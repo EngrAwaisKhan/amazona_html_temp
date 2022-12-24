00c3691f13340139f2a0ca6b14577c2f2a8bf20c
Muhammad Awais Khan <66188894+EngrAwaisKhan@users.noreply.github.com>
2022-12-24 18:36:10 +0500

    section-12(Video-20-Implement-Signin-Backend)

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import data from './data.js';
+import dotenv from 'dotenv';
 import userRouter from './routes/userRouter.js';
 
+dotenv.config();
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona_html_temp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URL || 'mongodb://localhost/amazona_html_temp',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 // ***PRODUCT API***
 // API for fetching Product by Id
 app.get('/api/products/:id', (req, res) => {
