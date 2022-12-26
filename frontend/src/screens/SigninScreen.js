dc12ae74cc4beed49c53757bd15275f9fc141204
Muhammad Awais Khan <66188894+EngrAwaisKhan@users.noreply.github.com>
2022-12-26 11:41:42 +0500

    section-12(Video-22-Implement-SignIn-Action)

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,20 +1,40 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { signin } from '../actions/userActions';
+import LoadingBox from '../Components/LoadingBox';
+import MessageBox from '../Components/MessageBox';
+import { useNavigate } from 'react-router-dom';
 
 export default function SigninScreen() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
+
+  const dispatch = useDispatch();
+
   const submitHandler = (e) => {
     e.preventDefault();
-    // TODO signin action
+    dispatch(signin(email, password));
   };
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/shiping');
+    }
+  }, [userInfo]);
+
   return (
     <div>
       <form className="form" onSubmit={submitHandler}>
         <div>
           <h1>Sign In</h1>
         </div>
+        {loading && <LoadingBox></LoadingBox>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email Address</label>
           <input
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,23 +1,30 @@
 import { compose, applyMiddleware, combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import {cartReducer} from './reducers/cartReducers';
+import { cartReducer } from './reducers/cartReducers';
 import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
+import { userSigninReducer } from './reducers/userReducers';
 
 const initialState = {
-  cart:{
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
+  cart: {
     cartItems: localStorage.getItem('cartItems')
-    ?JSON.parse(localStorage.getItem('cartItems'))
-    :[]
-  }
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  },
 };
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const store = createStore(reducer,initialState, composeEnhancer(applyMiddleware(thunk)),);
